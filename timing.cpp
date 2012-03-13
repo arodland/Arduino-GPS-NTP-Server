ed132656be67f978cb8666c2ab801bcddc84a68a
Andrew Rodland <andrew@cleverdomain.org>
2012-03-13 01:45:16 -0400

    Use a more accurate formula for sawtooth correction

--- timing.cpp
+++ timing.cpp
@@ -3,8 +3,8 @@
 
 #include <string.h>
 
-//#define SAWTOOTH_COMP 1
-#undef SAWTOOTH_COMP
+#define SAWTOOTH_COMP 1
+//#undef SAWTOOTH_COMP
 
 volatile /* static */ char ints = 0;
 /* For timing medium-resolution events like tempprobe reading and DHCP
@@ -37,11 +37,21 @@ const uint32 PLL_OFFSET_NS = 15625000L - PLL_FUDGE_NS;
 extern const int32 NTP_FUDGE_RX = PLL_OFFSET_NTP + (NTP_FUDGE_RX_US * 429497) / 100;
 extern const int32 NTP_FUDGE_TX = PLL_OFFSET_NTP + (NTP_FUDGE_TX_US * 429497) / 100;
 
+static inline uint32 sawtooth_ns(unsigned short tm) {
+  uint32 ns = (NSPC(tm) * tickadj_accum + 128) >> 8;
+  return ns;
+}
+
+static inline uint32 sawtooth_ntp(unsigned short tm) {
+  uint32 ntp = (NTPPC(tm) * tickadj_accum + 128) >> 8;
+  return ntp;
+}
+
 int32 make_ns(unsigned char i, unsigned short counter) {
   unsigned short tm = timer_get_interval();
   int32 ns = i * NS_PER_INT + counter * NSPC(tm);
 #ifdef SAWTOOTH_COMP
-  ns -= NSPADJ(tm) * tickadj_accum;
+  ns -= sawtooth_ns(tm);
 #endif
   if (ns + PLL_OFFSET_NS > 1000000000L) {
     ns -= 1000000000L;
@@ -54,7 +64,7 @@ int32 make_ns_carry(unsigned char i, unsigned short counter, char *add_sec) {
   unsigned short tm = timer_get_interval();
   int32 ns = i * NS_PER_INT + counter * NSPC(tm);
 #ifdef SAWTOOTH_COMP
-  ns -= NSPADJ(tm) * tickadj_accum;
+  ns -= sawtooth_ns(tm);
 #endif
   if (ns + PLL_OFFSET_NS > 1000000000L) {
     ns -= 1000000000L;
@@ -69,7 +79,7 @@ uint32 make_ntp(unsigned char i, unsigned short counter, int32 fudge) {
   unsigned short tm = timer_get_interval();
   uint32 ntp = i * NTP_PER_INT + counter * NTPPC(tm);
 #ifdef SAWTOOTH_COMP
-  ntp -= NTPPADJ(tm) * tickadj_accum;
+  ntp -= sawtooth_ntp(tm);
 #endif
   ntp += fudge;
   return ntp;
@@ -79,7 +89,7 @@ uint32 make_ntp_carry(unsigned char i, unsigned short counter, int32 fudge, char
   unsigned short tm = timer_get_interval();
   uint32 ntp = i * NTP_PER_INT + counter * NTPPC(tm);
 #ifdef SAWTOOTH_COMP
-  ntp -= NTPPADJ(tm) * tickadj_accum;
+  ntp -= sawtooth_ntp(tm);
 #endif
   uint32 ntp_augmented = ntp + fudge;
   *add_sec = ntp_augmented < ntp;
